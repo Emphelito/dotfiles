f76c06d97c3ab724966883324c013da25b155f20
emph <marcus@emph.dev>
2024-05-11 12:00:24 +0200

    upd

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "regex", "bash", "markdown", "markdown_inline" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "regex", "bash", "markdown", "markdown_inline", "nix" },
             sync_install = false,
             auto_install = true,
             ignore_install = { "latex" },
